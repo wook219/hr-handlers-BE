9ffceb54fe6d58e646f1889f6557d1f0124f407b
jsjhyun <gusrnl98@naver.com>
2024-11-19 17:03:12 +0900

    init

---
+++ src/main/java/com/hr_handlers/HrHandlersApplication.java
@@ -0,0 +1,13 @@
+package com.hr_handlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrHandlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrHandlersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hr_handlers/HrHandlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr_handlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrHandlersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
