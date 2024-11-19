cf1c1a1acee5a100ee8eb8130b7bedd49f1b0cd8
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-11-19 18:43:36 +0900

    CORS error 해결

--- src/main/java/com/hr_handlers/global/config/CorsConfig.java
+++ src/main/java/com/hr_handlers/global/config/CorsConfig.java
@@ -15,6 +15,5 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedHeaders("*") // 클라이언트가 서버로 보낼 때 허용되는 HTTP 헤더의 목록을 정의 현재는 모든 헤더 허용
                 // .exposedHeaders("Authorization")
                 .allowCredentials(true); // 클라이언트가 서버와 교환하는 요청에서 쿠키 또는 인증 관련 정보를 포함할 수 있게 허용
-
     }
 }
