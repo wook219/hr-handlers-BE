dca05fdf1c4c7fb363d3034d83edda561784022f
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-11-20 15:26:35 +0900

    Feat : 웹소켓 기본 설정

---
+++ src/main/java/com/hr_handlers/chat/websocket/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.hr_handlers.chat.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws") // socket 연결 엔드포인트
+                .setAllowedOrigins("*") // CORS 허용범위
+                .withSockJS(); // 브라우저 호환성을 위해
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue"); // 메시지를 구독 요청하는 엔드 포인트(public, private)
+        registry.setApplicationDestinationPrefixes("/app"); // 메시지를 발행하는 엔드 포인트
+    }
+}
