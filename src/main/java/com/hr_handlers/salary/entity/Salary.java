63d56b197db81d990e701bca8ae2013aaf1a6cf0
[CLOUD4] <dbslxl11@gmail.com>
2024-11-20 11:25:39 +0900

    Feat: 급여관리 엔티티 생성

---
+++ src/main/java/com/hr_handlers/salary/entity/Salary.java
@@ -0,0 +1,39 @@
+package com.hr_handlers.salary.entity;
+
+import com.hr_handlers.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Salary extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+//    @ManyToOne
+//    @JoinColumn(name = "employee_id", nullable = false)
+//    private Employee employee;
+
+    @Column(name = "basic_salary", nullable = false)
+    private int basicSalary;
+
+    @Column(name = "deduction", nullable = false)
+    private int deduction;
+
+    @Column(name = "net_salary", nullable = false)
+    private int netSalary;
+
+    @Column(name = "pay_date", nullable = false, columnDefinition = "TIMESTAMP")
+    private Timestamp payDate;
+}
