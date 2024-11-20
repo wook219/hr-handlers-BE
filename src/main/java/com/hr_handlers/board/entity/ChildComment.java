00c7294af832db805ac75ab2842f1c500ca04ccc
이서율 <yul9910@naver.com>
2024-11-20 11:03:47 +0900

    Feat: 게시판(게시글, 댓글) 엔티티 생성

---
+++ src/main/java/com/hr_handlers/board/entity/ChildComment.java
@@ -0,0 +1,33 @@
+package com.hr_handlers.board.entity;
+
+import com.hr_handlers.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "child_comment")
+@Entity
+public class ChildComment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pcomment_id", nullable = false)
+    private ParentComment parentComment;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "comment_content", columnDefinition = "TEXT", nullable = false)
+    private String commentContent;
+
+    @Column(name = "is_delete", length = 1, nullable = false)
+    private String isDelete;
+}
---
+++ src/main/java/com/hr_handlers/board/entity/Post.java
@@ -0,0 +1,40 @@
+package com.hr_handlers.board.entity;
+
+import com.hr_handlers.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "image_url", length = 1000)
+    private String imageUrl;
+
+    @Column(name = "is_delete", length = 1, nullable = false)
+    private String isDelete;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ParentComment> parentComments;
+}
