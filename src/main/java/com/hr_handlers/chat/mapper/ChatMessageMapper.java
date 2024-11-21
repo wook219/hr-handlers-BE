0cb80d1fedef62cf6418275412a4f15cf59557ec
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-11-21 14:26:03 +0900

    Feat : ChatMessageMapper 추가

---
+++ src/main/java/com/hr_handlers/chat/mapper/ChatMessageMapper.java
@@ -0,0 +1,17 @@
+package com.hr_handlers.chat.mapper;
+
+import com.hr_handlers.chat.dto.ChatMessageResponseDto;
+import com.hr_handlers.chat.entity.ChatMessage;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ChatMessageMapper {
+
+    // Entity -> DTO
+    @Mapping(source = "id", target = "messageId")  // id -> messageId
+    @Mapping(source = "chatRoom.id", target = "chatRoomId")  // chatRoom의 id -> chatRoomId
+    @Mapping(source = "employee.name", target = "employeeName") // employee의 name -> employeeName
+    @Mapping(source = "createdAt", target = "createdAt")
+    ChatMessageResponseDto toDto(ChatMessage chatMessage);
+}
