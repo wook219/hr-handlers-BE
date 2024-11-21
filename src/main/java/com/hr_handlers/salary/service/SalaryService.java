a27e1644a17eae7521d06eb1e81985fbd8b19fee
[CLOUD4] <dbslxl11@gmail.com>
2024-11-21 11:14:17 +0900

    refactor: 급여관리 공통 response 추가, api 제거

--- src/main/java/com/hr_handlers/salary/service/SalaryService.java
+++ src/main/java/com/hr_handlers/salary/service/SalaryService.java
@@ -1,5 +1,6 @@
 package com.hr_handlers.salary.service;
 
+import com.hr_handlers.global.dto.SuccessResponse;
 import com.hr_handlers.salary.dto.response.SalaryResponse;
 import com.hr_handlers.salary.repository.SalaryRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,7 @@ public class SalaryService {
 
     private final SalaryRepository salaryRepository;
 
-    public ResponseEntity<List<SalaryResponse>> getSalaryByUser() {
+    public SuccessResponse<List<SalaryResponse>> getSalaryByUser() {
 
         // todo: 로그인 기능 완료시 주석 내용 수정해서 employeeId 가져오기
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -23,6 +24,8 @@ public class SalaryService {
         Long employeeId = 1L;
 
         List<SalaryResponse> salaryResponse = salaryRepository.findSalaryByEmployeeId(employeeId);
-        return new ResponseEntity<>(salaryResponse, HttpStatus.OK);
+        return SuccessResponse.of(
+                "급여 관리 조회 성공",
+                salaryResponse);
     }
 }
