9f9b7c0424662688f5be8f85c3ad5c256e24ff0c
jsjhyun <gusrnl98@naver.com>
2024-11-22 18:29:12 +0900

    fix : ROLE 명 변경

--- src/main/java/com/hr_handlers/employee/enums/Role.java
+++ src/main/java/com/hr_handlers/employee/enums/Role.java
@@ -1,8 +1,8 @@
 package com.hr_handlers.employee.enums;
 
 public enum Role {
-    EMPLOYEE("ROLE_EMPLOYEE"), // 사원
-    ADMIN("ROLE_ADMIN");       // 관리자(인사관리팀)
+    ROLE_EMPLOYEE("사원"),
+    ROLE_ADMIN("관리자(인사관리팀)");
 
     private final String roleName;
 
