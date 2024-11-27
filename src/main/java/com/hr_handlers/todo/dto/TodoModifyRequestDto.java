be08b1919c1d72a54830b6beb1e33077d2655030
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-11-27 22:04:11 +0900

    Feat : Todo 유효성 검사 추가

--- src/main/java/com/hr_handlers/todo/dto/TodoModifyRequestDto.java
+++ src/main/java/com/hr_handlers/todo/dto/TodoModifyRequestDto.java
@@ -1,5 +1,7 @@
 package com.hr_handlers.todo.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +13,16 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class TodoModifyRequestDto {
 
+    @NotEmpty(message = "제목을 입력해주세요.")
+    @Size(min = 1, max = 20, message = "20자 이하로 입력해주세요.")
     private String title;
 
+    @Size(max = 200, message = "200자 이하로 입력해주세요.")
     private String content;
 
+    @NotEmpty(message = "시작 시간를 선택해주세요.")
     private Timestamp startTime;
 
+    @NotEmpty(message = "종료 시간를 선택해주세요.")
     private Timestamp endTime;
 }
--- src/main/java/com/hr_handlers/todo/dto/TodoRequestDto.java
+++ src/main/java/com/hr_handlers/todo/dto/TodoRequestDto.java
@@ -1,5 +1,7 @@
 package com.hr_handlers.todo.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +13,17 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class TodoRequestDto {
 
+    @NotEmpty(message = "제목을 입력해주세요.")
+    @Size(min = 1, max = 20, message = "20자 이하로 입력해주세요.")
     private String title;
 
+    @Size(max = 200, message = "200자 이하로 입력해주세요.")
     private String content;
 
+    @NotEmpty(message = "시작 시간를 선택해주세요.")
     private Timestamp startTime;
 
+    @NotEmpty(message = "종료 시간를 선택해주세요.")
     private Timestamp endTime;
 
     private Long employeeId;
