13bf22bbc9b25282e50ccd7c44f6ce3157f06ce8
[CLOUD4] <dbslxl11@gmail.com>
2024-12-02 21:33:24 +0900

    Feat: 엑셀 다운로드 api, excelUtil 구현

--- src/main/java/com/hr_handlers/global/utils/ExcelUtilMethodFactory.java
+++ src/main/java/com/hr_handlers/global/utils/ExcelUtilMethodFactory.java
@@ -1,17 +1,20 @@
 package com.hr_handlers.global.utils;
 
-import jakarta.servlet.http.HttpServletResponse;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 public interface ExcelUtilMethodFactory {
     public <T> List<T> parseExcelToObject(MultipartFile file, Class<T> clazz)  throws IOException;
+    public <T> void renderObjectToExcel(OutputStream stream, List<T> data, Class<T> clazz) throws IOException, IllegalAccessException;
+
 
     public <T> void parseHeader(Sheet sheet, Class<T> clazz);
     public <T> List<T> parseBody(Sheet sheet, Class<T> clazz);
+
+    public <T> void renderHeader(Sheet sheet, Class<T> clazz);
+    public <T> void renderBody(Sheet sheet, List<T> data, Class<T> clazz) throws IllegalAccessException;
 }
