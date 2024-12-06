0ab95e1c1fc610f5f99321b03c93d3bcb3a5254e
jsjhyun <gusrnl98@naver.com>
2024-12-06 11:34:19 +0900

    feat: 부서 관리 기능 추

--- src/main/java/com/hr_handlers/employee/repository/DeptRepository.java
+++ src/main/java/com/hr_handlers/employee/repository/DeptRepository.java
@@ -1,5 +1,6 @@
 package com.hr_handlers.employee.repository;
 
+import com.hr_handlers.admin.repository.employee.AdminDeptCustomRepository;
 import com.hr_handlers.employee.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface DeptRepository extends JpaRepository<Department, Long> {
+public interface DeptRepository extends JpaRepository<Department, Long>, AdminDeptCustomRepository {
     Optional<Department> findByDeptName(String deptName);
 
-   // SuccessResponse<String> register(String deptName);
+    boolean existsByDeptName(String deptName);
 }
