a1009032b0592c387abe68df73d23ce49decdd92
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-12-06 20:53:29 +0900

    Feat : 채팅 초대 목록 구현

--- src/main/java/com/hr_handlers/chat/repository/ChatCustomRepositoryImpl.java
+++ src/main/java/com/hr_handlers/chat/repository/ChatCustomRepositoryImpl.java
@@ -1,9 +1,14 @@
 package com.hr_handlers.chat.repository;
 
+import com.hr_handlers.chat.dto.ChatInviteResponseDto;
 import com.hr_handlers.chat.dto.ChatResponseDto;
 import com.hr_handlers.chat.entity.Chat;
 import com.hr_handlers.chat.entity.ChatRoom;
+import com.hr_handlers.chat.entity.QChat;
+import com.hr_handlers.employee.entity.QDepartment;
+import com.hr_handlers.employee.entity.QEmployee;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -61,6 +66,40 @@ public class ChatCustomRepositoryImpl implements ChatCustomRepository {
         } else {
             return existingChat;
         }
+    }
+
+    @Override
+    public List<ChatInviteResponseDto> findEmployeesNotInChat(Long chatRoomId, String keyword) {
+        QChat chat = QChat.chat;  // chat 객체 선언
+        QEmployee employee = QEmployee.employee;
+        QDepartment department = QDepartment.department; // Department 객체 선언
+
+        return jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                ChatInviteResponseDto.class,
+                                employee.empNo,
+                                employee.name,
+                                employee.position,
+                                employee.department.deptName
+                        )
+                )
+                .from(employee)
+                .leftJoin(employee.department, department)
+                .where(
+                        // Employee가 해당 채팅방에 참여하지 않은 경우
+                        JPAExpressions.selectFrom(chat)
+                                .where(
+                                        chat.chatRoom.id.eq(chatRoomId)  // 해당 chatRoomId에 해당하는 채팅방
+                                                .and(chat.employee.id.eq(employee.id))  // Employee가 해당 채팅방에 참여한 경우
+                                )
+                                .notExists(),
+
+                        // 키워드 검색 조건
+                        employee.name.likeIgnoreCase("%" + keyword + "%")
+                                .or(department.deptName.likeIgnoreCase("%" + keyword + "%"))
+                )
+                .fetch();
     }
 
     @Override
