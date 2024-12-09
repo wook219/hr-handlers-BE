2de04306519998d275944a2e858b1f893f9f032c
jsjhyun <gusrnl98@naver.com>
2024-12-09 17:51:25 +0900

    feat: 비밀번호 수정 분리(진행 중) - 보안 이슈

---
+++ src/main/java/com/hr_handlers/employee/dto/request/PasswordUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.hr_handlers.employee.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateRequestDto {
+
+    @NotBlank(message = "현재 비밀번호를 입력해주세요.")
+    private String currentPassword;
+
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    private String newPassword;
+
+    @NotBlank(message = "새 비밀번호 확인을 입력해주세요.")
+    private String confirmPassword;
+}
