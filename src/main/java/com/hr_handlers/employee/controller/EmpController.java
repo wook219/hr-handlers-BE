3bc033bec62b182b057d7a1e9c3f930385fc66c3
jsjhyun <gusrnl98@naver.com>
2024-12-09 11:54:16 +0900

    feat: 유효성 검사 추가

--- src/main/java/com/hr_handlers/employee/controller/EmpController.java
+++ src/main/java/com/hr_handlers/employee/controller/EmpController.java
@@ -6,6 +6,7 @@ import com.hr_handlers.employee.dto.response.EmpDetailResponseDto;
 import com.hr_handlers.employee.service.EmpService;
 
 import com.hr_handlers.global.dto.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -37,13 +38,13 @@ public class EmpController {
 
     // 비밀번호 찾기
     @PostMapping("/check")
-    public SuccessResponse<Boolean> matchEmailAndEmpNo(@RequestBody PasswordRecoveryRequestDto requestDto){
+    public SuccessResponse<Boolean> matchEmailAndEmpNo(@Valid @RequestBody PasswordRecoveryRequestDto requestDto){
                 return empService.matchEmailAndEmpNo(requestDto.getEmpNo(), requestDto.getEmail());
     }
 
     // 메일 전송
     @PostMapping("/send/mail")
-    public SuccessResponse<Boolean> sendResetPassword(@RequestBody PasswordRecoveryRequestDto requestDto){
+    public SuccessResponse<Boolean> sendResetPassword(@Valid @RequestBody PasswordRecoveryRequestDto requestDto){
         return empService.sendMail(empService.sendResetPassword(requestDto.getEmpNo(), requestDto.getEmail()));
     }
 }
