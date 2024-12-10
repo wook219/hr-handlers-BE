935959c6af810098d72cd0ab56650baa02177d3c
이서율 <yul9910@naver.com>
2024-12-10 22:29:55 +0900

    refactor: 파라미터 매핑 수정 및 스웨거 코드 추가

--- src/main/java/com/hr_handlers/global/controller/S3Controller.java
+++ src/main/java/com/hr_handlers/global/controller/S3Controller.java
@@ -1,6 +1,7 @@
 package com.hr_handlers.global.controller;
 
 import com.hr_handlers.global.service.S3Service;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -15,13 +16,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/s3")
 @RequiredArgsConstructor
+@Tag(name = "이미지", description = "이미지 S3 관련 API")
 public class S3Controller {
     private final S3Service s3Service;
 
     @PostMapping
     public ResponseEntity<?> uploadFile(
-            @RequestParam("path") String path,
-            @RequestParam("file") MultipartFile file) {
+            @RequestParam(value = "path") String path,
+            @RequestParam(value = "file") MultipartFile file) {
         try {
             String fileUrl = s3Service.uploadFile(path, file);
             return ResponseEntity.ok().body(Map.of(
@@ -37,7 +39,7 @@ public class S3Controller {
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteFile(@RequestParam("fileUrl") String fileUrl) {
+    public ResponseEntity<?> deleteFile(@RequestParam(value = "fileUrl") String fileUrl) {
         try {
             s3Service.deleteFile(fileUrl);
             return ResponseEntity.ok().body(Map.of("message", "파일 삭제 성공"));
