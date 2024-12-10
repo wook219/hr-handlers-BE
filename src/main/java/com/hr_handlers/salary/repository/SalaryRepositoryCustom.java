8d351d85a359c3804d77ff8110a80154105d4543
[CLOUD4] <dbslxl11@gmail.com>
2024-12-10 11:02:42 +0900

    Feat: 사용자 급여관리 api 추가

--- src/main/java/com/hr_handlers/salary/repository/SalaryRepositoryCustom.java
+++ src/main/java/com/hr_handlers/salary/repository/SalaryRepositoryCustom.java
@@ -2,10 +2,12 @@ package com.hr_handlers.salary.repository;
 
 
 import com.hr_handlers.salary.dto.response.SalaryResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface SalaryRepositoryCustom {
 
-    List<SalaryResponse> findSalaryByEmployeeId(Long employeeId);
+    Page<SalaryResponse> findSalaryByEmployeeNumber(Pageable pageable, String employeeNumber);
 }
